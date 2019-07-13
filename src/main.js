1991a3f73a6878d779de9458e7a832c5dfa4af04
piotrpopiolek <piotr.popiolek93@gmail.com>
2019-07-13 20:12:54 +0200

    Configured webpack and installed bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue';
+import '@scss//custom.scss';
 import router from './router';
 import App from '@components/App';
 import styles from '@scss/reset.scss';
 
+Vue.use(BootstrapVue);
+
 new Vue({
-    router,
-    render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app');
