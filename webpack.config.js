41c4800ea68e168ca52055bd7d42b83f1ae9be80
piotrpopiolek <piotr.popiolek93@gmail.com>
2019-07-13 19:14:21 +0200

    Changed host address

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
         compress: true,
-        host: '0.0.0.0',
+        host: 'localhost',
         port: 9000,
         open: true,
     },
