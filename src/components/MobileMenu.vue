99b906b5e076ebdcad7c649649f1a905f65ec18b
piotrpopiolek <piotr.popiolek93@gmail.com>
2019-07-15 18:28:34 +0200

    Added sidebar

--- src/components/MobileMenu.vue
+++ src/components/MobileMenu.vue
@@ -1,23 +1,63 @@
 <template>
   <div>
-    <div class="mobileMenu">
-      <router-link to="/about">About</router-link>
-      <router-link to="/contact">Contact</router-link>
+    <div :class="[isActive ? 'active sidebar' : 'sidebar']">
+      <div class="toggle-btn" @click="isActive = !isActive">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <ul>
+        <li>
+          <router-link to="/about">About</router-link>
+        </li>
+        <li>
+          <router-link to="/contact">Contact</router-link>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "mobileMenu"
+  name: "mobileMenu",
+  data() {
+    return {
+      isActive: false
+    };
+  }
 };
 </script>
 
 <style lang="scss" scoped>
-.mobileMenu {
-  background-color: red;
+.sidebar {
+  background-color: #f7f8f9;
   height: 100%;
   width: 150px;
   position: absolute;
+  font-size: 16px;
+  left: -150px;
+}
+
+.active {
+  left: 0px;
+}
+
+ul li {
+  list-style: none;
+  padding: 10px 0;
+}
+
+.toggle-btn {
+  position: absolute;
+  left: 165px;
+  top: 5px;
+}
+.toggle-btn span {
+  width: 20px;
+  height: 4px;
+  background-color: #999d9d;
+  display: block;
+  margin-top: 4px;
 }
 </style>
--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,66 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
-    entry: './src/main.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    mode: 'development',
-    devtool: '#source-map',
-    cache: true,
-    optimization: {
-        minimize: false,
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        host: 'localhost',
-        port: 9000,
-        open: true,
-    },
-    resolve: {
-        extensions: ['.js', '.vue'],
-        alias: {
-            '@components': path.resolve(__dirname, './src/components'),
-            '@pages': path.resolve(__dirname, './src/pages'),
-            '@img': path.resolve(__dirname, './src/assets/img'),
-            '@scss': path.resolve(__dirname, './src/assets/scss'),
-        },
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js?$/,
-                exclude: /(node_modules)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                    },
-                }
-            },
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader',
-            },
-            {
-                test: /\.svg$/,
-                loader: 'vue-svg-loader',
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    'vue-style-loader',
-                    'css-loader',
-                    'sass-loader',
-                ]
-            },
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            inject: false,
-        }),
-        new VueLoaderPlugin(),
-    ],
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  mode: 'development',
+  devtool: '#source-map',
+  cache: true,
+  optimization: {
+    minimize: false
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    host: 'localhost',
+    port: 9000,
+    open: true
+  },
+  resolve: {
+    extensions: ['.js', '.vue'],
+    alias: {
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@img': path.resolve(__dirname, './src/assets/img'),
+      '@scss': path.resolve(__dirname, './src/assets/scss')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.svg$/,
+        loader: 'vue-svg-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: ['vue-style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      inject: false
+    }),
+    new VueLoaderPlugin()
+  ]
 };
